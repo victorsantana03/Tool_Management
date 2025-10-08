72872caca6fa776a5108b0dc78619a9b28b533d3
victorsantana03 <viclucigei.123@gmail.com>
2025-10-08 17:25:29 -0300

    feat: add Loan table in the prisma

---
+++ prisma/migrations/20251008171123_add_loan_table/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "LoanStatus" AS ENUM ('EMPRESTADO', 'DEVOLVIDO', 'DANIFICADO');
+
+-- CreateTable
+CREATE TABLE "Loan" (
+    "id" TEXT NOT NULL,
+    "toolId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endDate" TIMESTAMP(3),
+    "status" "LoanStatus" NOT NULL DEFAULT 'EMPRESTADO',
+
+    CONSTRAINT "Loan_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Loan" ADD CONSTRAINT "Loan_toolId_fkey" FOREIGN KEY ("toolId") REFERENCES "Tool"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Loan" ADD CONSTRAINT "Loan_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
