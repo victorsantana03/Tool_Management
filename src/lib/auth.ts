a51f8a8bc65f90438e7e6235633515c4113495dd
victorsantana03 <viclucigei.123@gmail.com>
2025-10-10 16:33:15 -0300

    refactor: ensure user have a type

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,19 @@
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
+interface userType {
+  id: string;
+  email: string;
+  name: string;
+  role: "ADMIN" | "FUNCIONARIO";
+}
+
 export async function getUserFromToken() {
   const token = (await cookies()).get("token")?.value;
   if (!token) return null;
 
   try {
-    return jwt.verify(token, process.env.JWT_SECRET!) as {
-      id: string;
-      role: "ADMIN" | "FUNCIONARIO";
-    };
+    return jwt.verify(token, process.env.JWT_SECRET!) as userType;
   } catch {
     return null;
   }
